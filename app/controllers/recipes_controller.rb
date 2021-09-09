10c835bedc555e0d603fe895a69c2018ad395459
Artur <info@aigroupusa.com>
2021-09-09 15:44:22 -0400

    commit tree e590af611daebfe78695408c3712fe73523b1365

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe!, only: %i[show edit update]
+
   def show
-    @recipe = Recipe.find(params[:id])
   end
 
   def index
@@ -9,8 +10,32 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build[:id => '0']
+    @recipe.ingredients.build[:id => '1']
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+      if @recipe.save
+        redirect_to @recipe
+      end
+  end
+
+  def edit    
+  end
+
+  def update
+    @recipe.update(recipe_params)
+    redirect_to recipes_path 
+  end
+
+  private
+
+  def set_recipe!
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
   end
 end
